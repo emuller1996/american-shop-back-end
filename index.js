5044db837522d0546a6dfdcb3f190a64d72b24c3
emuller1996 <stefano.muller@hotmail.com>
2022-11-24 13:00:19 -0500

    change port to 3000

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const {
 
 // Syncing all the models at once.
 /* conn.sync({ force: true }).then(() => { */
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(3000, () => {
+    console.log('%s listening at 3000'); // eslint-disable-line no-console
   });
 /* }); */
