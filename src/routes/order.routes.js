c83596e81354dda049b5e9cfd8ae041647284d1b
emuller1996 <stefano.muller@hotmail.com>
2024-01-05 17:37:42 -0500

    fix webhooks

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -19,22 +19,23 @@ orderRouter.post("/webhooks", async (req, res) => {
   console.log(req.body);
   const data = req.body;
   if (data.action === "payment.updated") {
-    const payment_mercado = await axios.get(
-      `https://api.mercadopago.com/v1/payments/${data.data.id}`,
-      {
-        headers: { Authorization: `Bearer ${process.env.ACCESS_TOKEN}` },
-      }
-    );
-    console.log(payment_mercado);
     try {
+      const payment_mercado = await axios.get(
+        `https://api.mercadopago.com/v1/payments/${data.data.id}`,
+        {
+          headers: { Authorization: `Bearer ${process.env.ACCESS_TOKEN}` },
+        }
+      );
+      console.log(payment_mercado);
       const pago = await Payment.update(
         {
           status: payment_mercado.status,
-          net_received_amount:payment_mercado?.transaction_details?.net_received_amount,
+          net_received_amount:
+            payment_mercado?.transaction_details?.net_received_amount,
           net_amount: payment_mercado.transaction_amount,
-          fee_details_amount: payment_mercado?.fee_details[0]?.amount,
-          net_amount: payment_mercado.net_amount,
-          status_detail: payment_mercado.status_detail,
+          fee_details_amount: payment_mercado?.fee_details?.[0]?.amount,
+          net_amount: payment_mercado?.net_amount,
+          status_detail: payment_mercado?.status_detail,
         },
         { where: { id_pago_merca: data.data.id } }
       );
