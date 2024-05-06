37fefaf3fcc11a58763f6ed02ce5437789b7afbb
emuller1996 <stefano.muller@hotmail.com>
2024-05-06 09:09:57 -0500

    FIX SSL REQUIRED

--- src/db.js
+++ src/db.js
@@ -26,7 +26,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 // Configuración de la instancia de Sequelize
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
     logging: false,
     native: false,
